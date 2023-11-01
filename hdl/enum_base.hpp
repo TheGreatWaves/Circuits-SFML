a3caab4295c7a9ad7cb648bde7189425b9148310
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-11-01 22:52:43 +0700

    refactored enum_base

--- hdl/enum_base.hpp
+++ hdl/enum_base.hpp
@@ -23,17 +23,21 @@
  */
 
 #pragma once
-#ifndef TOKEN_BASE
-#define TOKEN_BASE
+#ifndef ENUM_BASE_CLASS
+#define ENUM_BASE_CLASS
 
 #include <string_view>
 #include <type_traits>
 
 /**
- * Inspired by the Carbon programming language enums.
+ * This enum implementation technique is heaviliy inspired by the Carbon programming language enums.
  */
 
-#define TOKEN_DECLARE_RAW_ENUM_CLASS(enum_class_name, underlying_type) \
+/**
+ * Use to declare a new raw enum class. Note that this also define 
+ * a list holding the name of each enum.
+ */
+#define DECLARE_RAW_ENUM_CLASS(enum_class_name, underlying_type) \
     namespace _private                                                 \
     {                                                                  \
     enum class enum_class_name##Raw : underlying_type;                 \
@@ -41,36 +45,49 @@
     }                                                                  \
     enum class _private::enum_class_name##Raw : underlying_type
 
-// Represents an enum entry.
-#define TOKEN_RAW_ENUM_ENUMERATOR(name) name,
-
-#define TOKEN_DEFINE_ENUM_CLASS_NAMES(enum_class_name) \
+/**
+ * Defines a list of strings holding the name for
+ * each entry of the enum. Note that this has to 
+ * be called after declaring a new enum class.
+ */
+#define DEFINE_ENUM_CLASS_NAMES(enum_class_name) \
 inline constexpr std::string_view _private::enum_class_name##Names[]
 
-// This can be used inside arrays to generate a list of enum names.
-#define TOKEN_ENUM_NAME(name) #name,
+/**
+ * ENUMERATORS.
+ */
 
-// This is used to generate a named constant for each enum values.
-#define TOKEN_ENUM_CONSTANT_DECLARATION(name) static const EnumType name;
+// Used to declare each value inside an enum class.
+#define RAW_ENUM_ENUMERATOR(name) name,
 
-#define TOKEN_ENUM_CONSTANT_DEFINITION(enum_class_name, name) \
+// Used to declare each name value for each enum value.
+#define ENUM_NAME_ENUMERATOR(name) #name,
+
+// Used to generate a named constant for each enum value.
+#define ENUM_CONSTANT_DECLARATION_ENUMERATOR(name) static const EnumType name;
+
+// Used to generate the definition for each named constant for each enum value.
+#define ENUM_CONSTANT_DEFINITION_ENUMERATOR(enum_class_name, name) \
  constexpr enum_class_name enum_class_name::name = enum_class_name::create(RawEnumType::name);
 
+/**
+ * The base class for Enums.
+ */
 #define ENUM_BASE(enum_class_name) \
  EnumBase<enum_class_name, _private::enum_class_name##Raw, _private::enum_class_name##Names>
 
 // The TokenBase class is simply a wrapper class for a enum type.
 // It utilizes X-macros for ease of extension.
-template <typename DerivedType, typename RawT, const std::string_view Names[]>
+template <typename DerivedT, typename RawT, const std::string_view Names[]>
 class EnumBase
 {
   public:
     using RawEnumType    = RawT;
-    using EnumType       = DerivedType;
+    using EnumType       = DerivedT;
     using UnderlyingType = std::underlying_type<RawT>;
 
     // Allow conversion to raw type.
-    constexpr operator RawEnumType() const
+    constexpr operator RawEnumType() const noexcept
     {
         return value;
     }
