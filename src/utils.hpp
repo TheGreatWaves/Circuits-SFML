3ae6380dce2c769b1113299ece28f60bc9a98f2d
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-04-18 16:45:16 +0700

    feat: add utility for applying bitmask and reading bitmask value

--- src/utils.hpp
+++ src/utils.hpp
@@ -26,10 +26,12 @@
 #ifndef UTILS
 #define UTILS
 
+#include <type_traits>
 #include <algorithm>
 #include <string_view>
 #include <cctype>
 #include <iostream>
+#include "pin.hpp"
 
 inline std::string make_lower(std::string_view str)
 {
@@ -64,6 +66,50 @@ inline void error(std::string_view dump)
 {
 	std::cout << "Error: " << dump << '\n';
 }
+
+template <typename ReturnType, typename DataType>
+inline std::enable_if_t<std::is_unsigned_v<ReturnType>, ReturnType> 
+bitvec_to_uint(const std::vector<DataType>& vec, const std::function<bool(const DataType&)> f, std::size_t start, std::size_t end)
+{
+	ReturnType val {0};
+
+	for (; start < end; start++)
+	{
+		val <<= 1;
+		val |= (f(vec[start])) ? 1 : 0;
+	}
+
+	return val;
+}
+
+template <typename ReturnType = std::size_t>
+inline std::enable_if_t<std::is_unsigned_v<ReturnType>, ReturnType> 
+pinvec_to_uint(const std::vector<Pin>& vec, std::size_t start, std::size_t end)
+{
+	auto pin_is_active = [](const Pin& p) { return p.is_active(); };
+	return bitvec_to_uint<ReturnType, Pin>(vec, std::move(pin_is_active), start, end);
+}
+
+template <typename T, typename VT>
+inline std::enable_if_t<std::is_unsigned_v<T>> 
+set_bitvec(T value, std::vector<VT>& target, std::function<void(VT&, bool)> f, std::size_t start, std::size_t end)
+{
+	std::size_t size = end - start;
+  for (std::size_t offset = 0; offset < size; offset++)
+  {
+		auto bit_on = (((value >> ((size-1) - offset) ) & 1) == 1);
+		f(target[start + offset], bit_on);
+  }
+}
+
+template <typename T>
+inline std::enable_if_t<std::is_unsigned_v<T>> 
+set_pinvec(T value, std::vector<Pin>& target, std::size_t start, std::size_t end)
+{
+	const auto f = [](Pin& pin, bool on){ pin.state = on ? PinState::ACTIVE : PinState::INACTIVE; };
+	set_bitvec<T, Pin>(value, target, std::move(f), start, end);
+}
+
 
 #define BLOCK "======"
 
