d43e861307e29b0bee3e8293a038723417bdb326
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-04-25 16:25:05 +0700

    feat: assembler token type

---
+++ src/lang/assembler/token_assembler.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef TOKEN_ASSEMBLER_H
+#define TOKEN_ASSEMBLER_H
+
+#define TOKEN_CLASS_NAME AssemblerTokenType
+#define TOKEN_DESCRIPTOR_FILE "../assembler/token_assembler.def"
+#include "../core/token_generator.hpp"
+
+#define TOKEN_CLASS_NAME AssemblerTokenType
+#define TOKEN_DESCRIPTOR_FILE "../assembler/token_assembler.def"
+#include "../core/scanner_generator.hpp"
+
+#endif /* TOKEN_TEST_H */
